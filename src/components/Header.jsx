2da52c33fa6b9d7477c25def1b8927add5bd1214
Heidy Florez <heidyflorez55@gmail.com>
2022-08-29 22:24:20 -0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/styles/components/Header.scss';
+import logo from '../assets/static/logo-platzi-video-BW2.png';
+import userIcon from '../assets/static/user-icon.png';
+
+const Header = () => (
+  <header className='header'>
+    {/* el link evita el refresh que genera la etiqueta a */}
+    <Link to='/'>
+      <img className='header__img' src={logo} alt='Platzi Video' />
+    </Link>
+    <div className='header__menu'>
+      <div className='header__menu--profile'>
+        <img src={userIcon} alt='' />
+        <p>Perfil</p>
+      </div>
+      <ul>
+        <li><a href='/'>Cuenta</a></li>
+        <li><Link to='/login'>Cerrar Sesión</Link></li>
+      </ul>
+    </div>
+  </header>
+);
+
+export default Header;
